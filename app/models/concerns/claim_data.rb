05585df2b9e528bfa20fa1dc4b269a90ccfcd4b4
Giridharan <giridharan.senthilkumar@adcltech.com>
2020-12-28 14:08:01 +0530

    Added rs:10 to check claims

--- app/models/concerns/claim_data.rb
+++ app/models/concerns/claim_data.rb
@@ -15,7 +15,7 @@ module ClaimData
 
 		def pending_claims(start_date = nil, end_date = nil, data = nil)
 			query = start_date && end_date ? where( Hash[ (has_attribute?(:ack_date) ? "ack_date" : "claim_date").to_sym, start_date..end_date]) : where(nil)
-			datas = query.where(amount_status:nil, claim_status: [nil, "0"]).where("ws_settle_amount IN ('') OR ws_settle_amount IS NULL OR ws_settle_amount::float < claim_amount::float")
+			datas = query.where(amount_status:nil, claim_status: [nil, "0"]).where("ws_settle_amount IN ('') OR ws_settle_amount IS NULL OR ws_settle_amount::float +10 < claim_amount::float")
 			null_amounts = datas.where(ws_settle_amount: [nil, ""])
 			null_amt_cnt = null_amounts.pluck('count(id), sum(claim_amount::float)').first
 			count_and_sum = datas.where.not(ws_settle_amount: [nil,""]).pluck("count(id), sum(claim_amount::float), sum(ws_settle_amount::float)").first 
@@ -24,7 +24,7 @@ module ClaimData
 
 		def setteld_claims(start_date = nil, end_date = nil)
 			query = start_date && end_date ? where( Hash[ (has_attribute?(:ack_date) ? "ack_date" : "claim_date").to_sym, start_date..end_date]) : where(nil)
-			datas = query.where(amount_status:nil, claim_status: "0").where.not(ws_settle_amount: "").where('ws_settle_amount::float >= claim_amount::float')
+			datas = query.where(amount_status:nil, claim_status: "0").where.not(ws_settle_amount: "").where('ws_settle_amount::float >= claim_amount::float -10')
 			count_and_sum = datas.pluck('count(id), sum(claim_amount::float)').first
 			return {count: count_and_sum[0].to_i, amount: count_and_sum[1].to_f.round(2), datas: datas}
 		end
